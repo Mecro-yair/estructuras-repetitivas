4eb2a512bfe4ff68a119b35e90e32d66d129910c
Yair292 <dluquep@unjbg.edu.pe>
2024-06-13 12:02:32 -0500

    Agrega ejercicio 1 de estructuras repetitivas

---
+++ Ejercicio_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main (){
+	int n,num = 7,i;
+	cout<<"Digite la cantidad de mutiplos de 7"<<endl; cin>>n;
+	for(i=1;i<=n;i++){
+		cout<<"El multiplo "<<i<<" es:"<<num*i<<endl;
+	}
+	return 0;
+}
+//"Si puedes imaginarlo,puedes programarlo"
