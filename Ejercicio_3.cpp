48e524b6da4a37746574e28db503cc025ae7748c
Yair292 <dluquep@unjbg.edu.pe>
2024-06-15 18:50:38 -0500

    Se añadio un error para datos invalidos

--- Ejercicio_3.cpp
+++ Ejercicio_3.cpp
@@ -3,14 +3,17 @@ using namespace std;
 
 int main() {
     int n;
-    cout << "Digite el n primeros numeros naturales a sumar : ";
-    cin >> n;
-
+    cout << "Digite el n primeros numeros naturales a sumar : "; cin >> n;
+    
     int suma = 0;
-    for (int i = 1; i <= n; ++i) {
-        suma =suma + i;
-    }
-    cout  << suma;
+    if (n>0){
+    	for (int i = 1; i <= n; ++i) {
+            suma =suma + i;
+        }
+        cout  << suma;
+	}else{
+		cout<<"TYPING_ERROR4";
+	}
     return 0;
 }
 //"Si puedes imaginarlo,puedes programarlo"
