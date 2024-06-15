c00a5250435b773afc01cfc8bf22d1efe3686531
Yair292 <dluquep@unjbg.edu.pe>
2024-06-15 00:29:22 -0500

    Se Actualizo los errores en caso de digitar texto

--- Ejercicio_2.cpp
+++ Ejercicio_2.cpp
@@ -2,13 +2,25 @@
 using namespace std;
 int main() {
     int num;
+	string stringnum;
     cout << "Digite un numero : "; cin >> num;
+    
+    stringnum=to_string(num);
+    
+        if (stringnum=="0"){
+	  	cout<<"ERROR2_STRING_INVALID";
+	  	return 0;
+	    }   
     while (num >= 0) {
-        cout << "Introduce otro numero  "<<endl;
+        cout << "Introduce otro numero: "<<endl;
         cin >> num;
+        stringnum=to_string(num);
+        if (stringnum=="0"){
+	  	cout<<"ERROR2_STRING_INVALID";
+	  	return 0;
+	    }   
     }
-    cout << "Se acabo,Haz digitado un numero negativo";
-
+    cout << "ERROR3_NEGATIVE_VALUE";
     return 0;
 }
 //"Si puedes imaginarlo,puedes programarlo"
