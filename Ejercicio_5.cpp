570dd9f3b4150e2a3d02a1ea7ce9fa3afae2633e
Yair292 <dluquep@unjbg.edu.pe>
2024-06-17 21:39:55 -0500

    Ejercicio_5:Se mejoro la condicional y soluciono para casos negativos

--- Ejercicio_5.cpp
+++ Ejercicio_5.cpp
@@ -1,32 +1,38 @@
 #include <iostream>
+#include <conio.h>
+
 using namespace std;
 
 int main() {
-    char respuesta = 'S'; // Inicializa respuesta a 'S'
+    string respuesta="si"; // Inicializa respuesta a 'S'
     float nota, promedio,suma = 0,contador = 0;
 
     int aprobados = 0;
     int desaprobados = 0;
-
-    while (respuesta !='N') { 
-        cout << "Digite la nota: ";
-        cin >> nota;
-        suma += nota;
+    while (respuesta =="si" || respuesta=="Si"|| respuesta=="SI" || respuesta=="sI") { 
+        cout << "Digite la nota: \n";cin >> nota;
+        if (nota>0){
+        	suma += nota;
         contador++;
-        if (nota > 10.5) {
+        if (nota > 10.5 && nota<=20) {
             aprobados++;
         } else {
             desaprobados++;
         }
-        cout << "¿Continuar? (S/N): ";
+        cout << "¿Continuar? (si/no): \n";
         cin >> respuesta;
+        }else{
+        	cout<<"Digite un numero positivo\n";
+		}
     }
+        
 
     promedio = suma / contador; 
+    
     cout << "Los aprobados son: " << aprobados << endl;
     cout << "Los desaprobados son: " << desaprobados << endl;
     cout << "El promedio de las notas del salón es: " << promedio << endl;
-
+    getch();
     return 0;
 }
 //"Si puedes imaginarlo,puedes programarlo"
